d8592821f5e4c42968bdd92f4db83a6bc6c56454
SNKolev18 <SNKolev18@codingburgas.bg>
2022-10-25 15:32:44 +0300

    Implement named pipe server and client

--- PipeClient/Program.cs
+++ PipeClient/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace PipeClient
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Security.Principal;
+using System.Text;
+
+namespace PipeClient
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var client = new NamedPipeClientStream(".", "ServerPipe", PipeDirection.InOut, PipeOptions.None, TokenImpersonationLevel.Impersonation);
+
+            Console.WriteLine("Connecting to server...");
+            client.Connect();
+
+            while (true)
+            {
+                Console.WriteLine("Enter text: ");
+                byte[] bytes = System.Text.Encoding.UTF8.GetBytes(Console.ReadLine());
+
+                client.Write(bytes, 0, bytes.Length);
+            }
+
+            
+
         }
     }
 }
--- PipeServer/Program.cs
+++ PipeServer/Program.cs
@@ -1,10 +1,47 @@
-﻿namespace PipeServer
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Text;
+
+namespace PipeServer
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+
+                var server = new NamedPipeServerStream("ServerPipe", PipeDirection.InOut, 10);
+                Console.WriteLine("Waiting for connections...");
+                server.WaitForConnection();
+
+                string username = server.GetImpersonationUserName();
+                Console.WriteLine("Connection received");
+                Console.WriteLine($"A client has connected: {username}");
+
+                while (true)
+                {
+                    byte[] buffer = new byte[256];
+                    int length = server.Read(buffer, 0, buffer.Length);
+
+                    if(length == 0)
+                    {
+                        Console.WriteLine("End of stream reached");
+                        break;
+                    }
+                    Console.WriteLine($"Received bytes {length}");
+
+                    string text = Encoding.UTF8.GetString(buffer, 0, length);
+                    Console.WriteLine(text);
+                }
+
+                server.Disconnect();
+            }
+            catch (IOException ioe)
+            {
+                Console.WriteLine($"Exception appeared: {ioe.Message}");
+            }
         }
     }
 }
