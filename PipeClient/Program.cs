b6e3f3f351b225d12c51ab1238ee90a47337c835
SNKolev18 <SNKolev18@codingburgas.bg>
2022-10-25 11:09:40 +0300

    Add project files.

---
+++ PipeClient/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PipeClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
